b77ac8dea83142d40ef107de6eaa0fa124aacbd4
Cedric Nagata <cedric.nagata@gideontaylor.com>
2026-04-20 13:14:59 -0700

    Initial Commit

---
+++ bigbro-test/bigbro_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  bigbro_testApp.swift
+//  bigbro-test
+//
+//  Created by Cedric Nagata on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct bigbro_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bigbro-testUITests/bigbro_testUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  bigbro_testUITestsLaunchTests.swift
+//  bigbro-testUITests
+//
+//  Created by Cedric Nagata on 4/20/26.
+//
+
+import XCTest
+
+final class bigbro_testUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
